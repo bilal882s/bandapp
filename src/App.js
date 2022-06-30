b76bde47573b0a55cf61905d30e1610704008d2e
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-06-30 10:34:03 +0500

    app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.scss';
 import Navbar from './components/header/Header';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/* <hr />
-      <div className="ico"></div> */}
+      <LandingPage />
     </div>
   );
 }
---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function LandingPage() {
+    return (
+        <div className='bg-image'>
+            <div className="container w-50 m-4">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-body">
+                                <h1>This is h1 tag</h1>
+                                <p>This is p tag</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
