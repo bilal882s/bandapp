c02399844647e7978f7a5970da1e419d3d9bec6b
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-06-30 23:03:56 +0500

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function LandingPage() {
     return (
-        <div className='bg-image '>
+        <div className='bg-image'>
             <div className="container w-50 mt-0 mx-5">
                 <div className="row">
                     <div className="col">
-                        <div className="card customcard">
+                        <div className="card bg-custom">
                             <div className="card-body">
-                                <h1>Get a $400 bouns then make it btter.</h1>
+                                <h1>Get a $400 bouns then make it better.</h1>
                                 <p>Create an account to save your salery on <b><i>My Bank</i></b></p>
                                 <button className="btn btn-danger">Go to Accounts</button>
                             </div>
