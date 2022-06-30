06982248e3d419828ee5d82d944693cdbacc6e09
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-06-30 10:08:30 +0500

    Navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,30 +4,15 @@ import Navbar from './Navbar';
 export default function Header() {
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dprimary" style={{ height: "2.6rem" }}>
-                <div class="container">
-                    <a class="navbar-brand" href="#">Date and time</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" style={{ flexGrow: "0" }} id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Personal  </a>
-                            </li>
-                            <li class="nav-item">
-                                 <a class="nav-link active" href="#">Smart Bussiness</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Commercial</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Wealth</a>
-                            </li>
-                        </ul>
+            <div className="topbar bg-dprimary text-white">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <div className="topbar text-white navbar-brand">Date and Time</div>
+                        </div>
                     </div>
                 </div>
-            </nav>
+            </div>
             <Navbar />
         </>
     )
--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -3,26 +3,24 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-header">
+                <div class="container">
                     <a class="navbar-brand" href="#">My Bank</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="collapse navbar-collapse" style={{ flexGrow: 0 }} id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Dashboard</a>
+                                <a class="nav-link active" href="#">Dashboard</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#"></a>
+                            <a class="nav-link active" href="#">Transactions</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled">Disabled</a>
-                            </li>
+                            <button className="btn btn-info mx-2">Sign Up</button>
                         </ul>
                     </div>
                 </div>
