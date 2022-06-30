33422b94895f58e147ed595bc0caa1e62f2c0aab
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-06-30 23:03:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Routing from './components/Routing/Routing';
+import Signup from "./pages/signupPage/Signup";
 
 function App() {
   return (
     <>
       <Routing />
+      {/* <Signup /> */}
       {/* <Dashboard /> */}
       {/* <Header />
       <LandingPage /> */}
