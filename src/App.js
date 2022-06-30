f813e25a70ded30094b1e88f528f874d0555909a
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-06-30 09:10:12 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Navbar from './components/header/Header';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* <hr />
+      <div className="ico"></div> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+export default function Header() {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dprimary" style={{ height: "2.6rem" }}>
+                <div class="container">
+                    <a class="navbar-brand" href="#">Date and time</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" style={{ flexGrow: "0" }} id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Personal  </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Smart Bussiness</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Commercial</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Wealth</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <Navbar />
+        </>
+    )
+}
