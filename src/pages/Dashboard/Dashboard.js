d003b4e9f8ef4065d0a684efe7f5ab70f8b8cfa4
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-01 15:48:20 +0500

    Update Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -10,10 +10,10 @@ export default function Dashboard() {
           <div className="col-12 col-md-6 col-lg-6">
             <div class="card shadow text-center">
               <div class="card-body">
-                <h5 class="card-title"><i class="fa-solid fa-user"></i>Accounts</h5>
+                <h5 class="card-title"><i class="fa-solid icon fa-user"></i>Accounts</h5>
                 <hr />
-                <button class="btn btn-primary p-1"><i class="fa-solid fa-plus p-1"></i>Add New User</button>
-                <button class="btn btn-secondary ms-3 p-1"><i class="fa-solid fa-eye p-1"></i>View All Accounts</button>
+                <button class="btn btn-primary p-1"><i class="fa-solid icon fa-plus p-1"></i>Add New User</button>
+                <button class="btn btn-secondary ms-3 p-1"><i class="fa-solid icon fa-eye p-1"></i>View All Accounts</button>
                 <hr />
                 <br />
                 <h1>0</h1>
@@ -21,12 +21,12 @@ export default function Dashboard() {
               </div>
             </div>
           </div>
-          <div className="col-12 col-md-6 col-lg-6 shadow">
-            <div class="card  text-center">
+          <div className="col-12 col-md-6 col-lg-6">
+            <div class="card shadow text-center">
               <div class="card-body">
-                <h5 class="card-title"><i class="fa-solid fa-money-bill-1"></i>Transactions</h5>
+                <h5 class="card-title"><i class="fa-solid icon fa-money-bill-1"></i>Transactions</h5>
                 <hr />
-                <button class="btn btn-secondary ms-3 p-1"><i class="fa-solid fa-eye p-1"></i>View All Accounts</button>
+                <button class="btn btn-secondary ms-3 p-1"><i class="fa-solid icon fa-eye p-1"></i>View All Accounts</button>
                 <hr />
                 <br />
                 <h1>0</h1>
