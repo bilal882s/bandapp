e228f150b395e393972e41c670c174b05982e63b
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-01 11:18:21 +0500

    Update Navbar.js

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
             <Header />
             <nav class="navbar navbar-expand-lg navbar-dark bg-header">
                 <div class="container">
-                    <a class="navbar-brand" href="#">My Bank</a>
+                    <Link to="/" class="navbar-brand">My Bank</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
