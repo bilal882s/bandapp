95c062fb1d43c308394884189ffb3cc87bec2468
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-02 12:36:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.scss';
+import 'react-toastify/dist/ReactToastify.css';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Routing from './components/Routing/Routing';
 import Signup from "./pages/signupPage/Signup";
-import Login from "../src/pages/signupPage/Login"
+import Login from "../src/pages/signupPage/Login";
 
 function App() {
   return (
