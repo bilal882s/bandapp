55e57c4b72024738f40258319ef2196e4f3fd646
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-02 12:37:09 +0500

    Create index.js

---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Navbar from '../../components/header/Navbar';
+import LandingPage from './LandingPage';
+export default function index() {
+    return (
+        <>
+            {/* <Navbar /> */}
+            {/* <Routes> */}
+                <Route path='/' element={<LandingPage />} />
+            {/* </Routes> */}
+        </>
+    )
+}
