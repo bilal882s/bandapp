ae38a1425d9ba591e0fd929cf614ac902f923585
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-03 13:13:50 +0500

    Update Transactions.js

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -3,18 +3,21 @@ import { Link } from 'react-router-dom'
 
 export default function Transactions() {
   return (
-    <div className="container center">
-      <div className="row w-100">
-        < div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-          <div className="card border border-1 border-black text-center" >
-            <Link to="/dashboard" className='Link'>Back to Dashboard</Link>
-            <h3 class="card-title"><i class="fa-solid icon fa-money-bill-1"></i>Transactions</h3>
-            <hr />
-            <h3>You didn't make a transaction yet</h3>
-            <hr />
+    <>
+      <div className="container center">
+        <Link to="/" className="btn btn-success m-2">Back To Home</Link>
+        <div className="row w-100">
+          <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+            <div className="card border border-1 border-black text-center" >
+              <Link to="/dashboard" className='Link'>Back to Dashboard</Link>
+              <h3 class="card-title"><i class="fa-solid icon fa-money-bill-1"></i>Transactions</h3>
+              <hr />
+              <h3>You didn't make a transaction yet</h3>
+              <hr />
+            </div>
           </div>
-        </div>
+        </div >
       </div >
-    </div >
+    </>
   )
 }
