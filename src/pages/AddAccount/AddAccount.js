8453d4fa75abac531f0a2bacb8117955a11ac9f5
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-03 13:14:12 +0500

    Create AddAccount.js

---
+++ src/pages/AddAccount/AddAccount.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AddAccount() {
+  return (
+    <>
+      <div className="container center border border-1 border-secondary">
+        <div className="row">
+          <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 my-5">
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
