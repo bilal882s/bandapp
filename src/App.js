2446b6552312d7b72933eb703765ad74583aace4
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-03 13:14:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-import Routing from './components/Routing/Routing';
+import Routing from './components/Routes/Routing';
 import Signup from "./pages/signupPage/Signup";
 import Login from "../src/pages/signupPage/Login";
 
 function App() {
   return (
-    <>
+    <div className='text-center'>
       <Routing />
       {/* <Login /> */}
       {/* <Signup /> */}
       {/* <Dashboard /> */}
       {/* <Header />
       <LandingPage /> */}
-    </>
+    </div>
   );
 }
 
