cb5740e72a9ad0835e0bba8e4e76800575a38fdc
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-03 23:40:29 +0500

    Update Navbar.js

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -6,25 +6,25 @@ export default function Navbar() {
     return (
         <>
             <Header />
-            <nav class="navbar navbar-expand-lg navbar-dark bg-header">
-                <div class="container">
-                    <Link to="/" class="navbar-brand">My Bank</Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-header">
+                <div className="container">
+                    <Link to="/" className="navbar-brand">My Bank</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" style={{ flexGrow: 0 }} id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <Link class="nav-link active" to="/">Home</Link>
+                    <div className="collapse navbar-collapse" style={{ flexGrow: 0 }} id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/">Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link active" to="/dashboard">Dashboard</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/dashboard">Dashboard</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link active" to="/transactions">Transactions</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/transactions">Transactions</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link active" to="/signup">Sign Up</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/signup">Sign Up</Link>
                             </li>
                         </ul>
                     </div>
