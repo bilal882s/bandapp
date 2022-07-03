7786555e1d680566c1a8618e72f9f54f533dca4c
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-03 13:14:14 +0500

    Update Routing.js

--- src/components/Routes/Routing.js
+++ src/components/Routes/Routing.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from '../../pages/LandingPage/index';
+import Dashboard from "../../pages/Dashboard/index";
+import Transactions from "../../pages/Transactions/Transactions"
+import Signup from '../../pages/signupPage/Signup';
+import Login from '../../pages/signupPage/Login';
+import DashboardMenu from '../../pages/Dashboard/DashboardMenu';
+import AddAccount from '../../pages/AddAccount/AddAccount';
 
 export default function Routing() {
   return (
-    <>
-        
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<LandingPage />} />
+        <Route path='/adduser' element={<AddAccount />} />
+        <Route path="/dashboard" element={<DashboardMenu />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/transactions' element={<Transactions />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
