193e4ec287919c22c843254c31651e525d0a5a50
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-04 21:48:16 +0500

    Update Nopage.js

--- src/pages/Nopage/Nopage.js
+++ src/pages/Nopage/Nopage.js
@@ -1,9 +1,7 @@
-import React from 'react'
-
 export default function Nopage() {
   return (
     <div className='text-center text-danger'>
-        <h1 className='m-3'>ERROR 404 PAGE NOT FOUND</h1>
+      <h1 className='m-3'>ERROR 404 PAGE NOT FOUND</h1>
     </div>
   )
 }
