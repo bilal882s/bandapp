666acb02d9ac8991bf2d7b2f5674ba43acfcc2ab
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-04 21:48:19 +0500

    Update index.js

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Navbar from '../../components/header/Navbar';
+import Nopage from '../Nopage/Nopage';
 import LandingPage from './LandingPage';
 export default function index() {
     return (
         <>
             <Navbar />
             <Routes>
-                <Route path='/'>
-                    <Route index element={<LandingPage />} />
-                </Route>
+                <Route index element={<LandingPage />} />
+                <Route path='*' element={<Nopage />} />
             </Routes>
         </>
     )
