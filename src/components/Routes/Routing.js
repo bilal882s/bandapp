7113500882567929fd452d32254d94ea80ca6fde
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-04 21:48:40 +0500

    Update Routing.js

--- src/components/Routes/Routing.js
+++ src/components/Routes/Routing.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from '../../pages/LandingPage/index';
-import Dashboard from "../../pages/Dashboard/index";
 import Transactions from "../../pages/Transactions/Transactions"
 import Signup from '../../pages/signupPage/Signup';
 import Login from '../../pages/signupPage/Login';
-import DashboardMenu from '../../pages/Dashboard/DashboardMenu';
+import DashboardMenu from '../../pages/Dashboard/Dashboard';
 import AddAccount from '../../pages/AddAccount/AddAccount';
 import Forget from '../../pages/signupPage/Forget';
+import Accounts from "../../pages/Dashboard/Accounts"
+import DashboardRoute from './DashboardRoute';
 
 export default function Routing() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/*' element={<LandingPage />} />
-        <Route path='/adduser' element={<AddAccount />} />
-        <Route path="/dashboard" element={<DashboardMenu />} />
-        <Route path='/signup' element={<Signup />} />
+        {/* <Route path='/dashboard/*' element={<DashboardRoute />} /> */}
         <Route path='/transactions' element={<Transactions />} />
+        <Route path='/dashboard' element={<DashboardMenu />} />
+        <Route path='/adduser' element={<AddAccount />} />
         <Route path="/login" element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
         <Route path="/forget" element={<Forget />} />
+        <Route path='/dashboard/accounts' element={<Accounts />} />
       </Routes>
     </BrowserRouter>
   )
