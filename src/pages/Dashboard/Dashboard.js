567e0328b6f73933841579c01c9a788ac5994eac
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-04 21:48:25 +0500

    Update Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,40 +1,28 @@
 import React from 'react';
-import DashboardMenu from "./DashboardMenu";
-import { Link } from "react-router-dom"
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
+import PersonIcon from '@mui/icons-material/Person';
+import DashboardMenu from "../Dashboard/DashboardMenu"
 
 export default function Dashboard() {
   return (
     <>
-      <Link to="/" className="btn btn-success m-2">Back To Home</Link>
+      <DashboardMenu />
       <div className='center' style={{ height: "60vh" }}>
         <h1 className="text-center">Dashboard Page</h1>
         <div className="container mt-3 text-center">
-          <div className="row d-flex">
-            <div className="col-12 col-md-6 col-lg-6">
-              <div class="card shadow text-center">
-                <div class="card-body">
-                  <h5 class="card-title"><i class="fa-solid icon fa-user"></i>Accounts</h5>
-                  <hr />
-                  <Link class="btn btn-primary m-sm-2 p-1" to="/adduser"><i class="fa-solid icon fa-plus p-1"></i>Add New User</Link>
-                  <button class="btn btn-secondary m-sm-2 ms-3 p-1"><i class="fa-solid icon fa-eye p-1"></i>View All Accounts</button>
-                  <hr />
-                  <br />
-                  <h1>0</h1>
-                  <h4>Accounts</h4>
-                </div>
-              </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-6">
-              <div class="card shadow text-center">
-                <div class="card-body">
-                  <h5 class="card-title"><i class="fa-solid icon fa-money-bill-1"></i>Transactions</h5>
-                  <hr />
-                  <button class="btn btn-secondary m-xs-2 m-sm-2 ms-3 p-1"><i class="fa-solid icon fa-eye p-1"></i>View All Accounts</button>
-                  <hr />
-                  <br />
-                  <h1>0</h1>
-                  <h4>Transactions</h4>
-                </div>
+          <div className="row">
+            <div class="card shadow text-center">
+              <div class="card-body">
+                <h5 class="card-title"><i class="fa-solid icon fa-money-bill-1"></i>Transactions</h5>
+                <hr />
+                <button class="btn btn-secondary m-xs-2 m-sm-2 ms-3 p-1"><i class="fa-solid icon fa-eye p-1"></i>View All Accounts</button>
+                <hr />
+                <br />
+                <h1>0</h1>
+                <h4>Transactions</h4>
               </div>
             </div>
           </div>
