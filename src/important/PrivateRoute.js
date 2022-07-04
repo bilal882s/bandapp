239f92077c1cf6e79802d557e9b4cc6c69627553
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-04 21:48:34 +0500

    Create PrivateRoute.js

---
+++ src/important/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/Authcontext'
+import Login from "../pages/signupPage/Login"
+
+export default function PrivateRoute({ Component }) {
+
+    const { isAuthenticated } = useContext(AuthContext)
+
+    console.log(Component)
+    console.log(isAuthenticated)
+
+    if (!isAuthenticated)
+        return <Login />
+
+    return (
+        <Component />
+    )
+}
