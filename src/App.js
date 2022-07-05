b2c008d93753a3a4d4662c01a5575f1db3fa01db
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-05 16:54:45 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import 'react-pro-sidebar/dist/css/styles.css';
 import 'react-toastify/dist/ReactToastify.css';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Routing from './components/Routes/Routing';
