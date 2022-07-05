6e1df69627117073fcc3a8369aeefbd92fb9f27e
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-05 16:55:58 +0500

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function LandingPage() {
     return (
         <div className='bg-image text-white shadow-lg'>
-            <div className="w-50 mt-0 mx-5">
-                <div className="row">
-                    <div className="col-12 col-md-12">
+            <div className="container w-50 mx-5">
+                <div className="row ">
+                    <div className="col-12 col-md-12 m-4">
                         <div className="card bg-custom">
                             <div className="card-body">
-                                <h1>Get a $400 bouns then make it better.</h1>
-                                <p>Create an account to save your salery on <b><i>My Bank</i></b></p>
+                                <h2>Get a $400 bouns then make it better.</h2>
+                                <p>Create an account on <b><i>My Bank</i></b></p>
                                 <Link to="/dashboard" className="btn btn-danger">Go to Dashboard</Link>
                             </div>
                         </div>
