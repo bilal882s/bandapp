0f9a09ea5b7c054f03feb887b0019fcde31fd09a
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-05 16:56:19 +0500

    Update Transactions.js

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -1,23 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import DashboardMenu from '../Dashboard/DashboardMenu';
 export default function Transactions() {
   return (
-    <>
-      <div className="container center">
-        <Link to="/" className="btn btn-success m-2">Back To Home</Link>
-        <div className="row w-100">
-          <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-            <div className="card border border-1 border-black text-center" >
-              <Link to="/dashboard" className='Link'>Back to Dashboard</Link>
-              <h3 class="card-title"><i class="fa-solid icon fa-money-bill-1"></i>Transactions</h3>
-              <hr />
-              <h3>You didn't make a transaction yet</h3>
-              <hr />
-            </div>
+    <div className='d-flex'>
+      <DashboardMenu />
+      <div className='center w-100' style={{ height: "60vh" }}>
+        <h1 className="text-center">Add / View Accounts</h1>
+        <div className="container mt-3 text-center">
+          <div className="row">
+            <Card className="col-12 col-md-12 col-lg-10 offset-lg-1 shadow ">
+              <div class="ftext-center">
+                <CardContent>
+                  <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Accounts</h5>
+                  <hr />
+                  <Button className='m-1' size="sm" variant={'contained'} color="success">
+                    <Link className='nav-link' to="/adduser" >Add New Account</Link>
+                  </Button>
+                  <Button className='m-1' size="sm" variant={'contained'} color="warning">
+                    <Link className='nav-link' to="/adduser" color='success'>View All Accounts</Link>
+                  </Button>
+                  <hr />
+                  <br />
+                  <h1>0</h1>
+                  <h4>Accounts</h4>
+                </CardContent>
+              </div>
+            </Card>
           </div>
-        </div >
-      </div >
-    </>
+        </div>
+      </div>
+    </div>
   )
 }
