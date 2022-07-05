6022c35fa0be4859461d6e23cc4b65fbfd5ba6d1
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-05 16:55:48 +0500

    Update Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,35 +1,56 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import { Link } from "react-router-dom";
-import PersonIcon from '@mui/icons-material/Person';
 import DashboardMenu from "../Dashboard/DashboardMenu"
 
 export default function Dashboard() {
   return (
-    <>
+    <div className='d-flex'>
       <DashboardMenu />
-      <div className='center' style={{ height: "60vh" }}>
+      <div className='m-5' style={{ height: "60vh", width: "60rem" }}>
         <h1 className="text-center">Dashboard Page</h1>
         <div className="container mt-3 text-center">
           <div className="row">
-            <div class="card shadow text-center">
-              <div class="card-body">
-                <h5 class="card-title"><i class="fa-solid icon fa-money-bill-1"></i>Transactions</h5>
-                <hr />
-                <Button className='m-1' size="sm" variant={'contained'} color="success">View All Dashboard
-                </Button>
-                <Button variant="outlined" color="secondary" >Click Me</Button>
-                <hr />
-                <br />
-                <h1>0</h1>
-                <h4>Transactions</h4>
-              </div>
+            <div className="col">
+              <table class="table w-100">
+                <thead className='table-dark'>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Accounts</th>
+                    <th scope="col">Transaction</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">Bilal</th>
+                    <td>0</td>
+                    <td>0</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Awais</th>
+                    <td>4</td>
+                    <td>898</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Ali</th>
+                    <td>1</td>
+                    <td>300</td>
+                  </tr>
+                  {/* <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr> */}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
