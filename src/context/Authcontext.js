e609ce9cc03e84fd54a5117cc4eafee2fb63c5ad
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-06 09:41:42 +0500

    Update Authcontext.js

--- src/context/Authcontext.js
+++ src/context/Authcontext.js
@@ -1,10 +1,24 @@
-import React, { createContext, useState } from 'react'
-
+import React, { createContext, useState, useEffect } from 'react'
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase";
 export const AuthContext = createContext()
 
 export default function AuthContextProvider({ children }) {
 
     const [isAuthenticated, setIsAuthenticated] = useState(false)
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                setIsAuthenticated(true)
+
+            } else {
+                setIsAuthenticated(false)
+                // console.log("user is not logged in ");
+
+            }
+        })
+
+    }, [])
 
     return (
         <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
