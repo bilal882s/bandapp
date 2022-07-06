38cc98d61434fd14d0ef9e1fbedbfcc8f3a50f46
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-06 09:41:34 +0500

    Update Navbar.js

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
 import Header from './Header';
-
+import { AuthContext } from '../../context/Authcontext';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from "firebase/auth";
+import { auth } from "../../config/firebase";
+import { toast } from 'react-toastify';
 
 export default function Navbar() {
+    // const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext)
+
+    const navigate = useNavigate();
+
+    const handleSignOut = (e) => {
+        // setIsAuthenticated(false);
+        signOut(auth).then(() => {
+            toast.success('You Have Successfully Logged Out', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }).catch((error) => {
+            toast.error('Something else here!', {
+                position: "top-left",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        });
+        // navigate('/');
+    }
     return (
         <>
             <Header />
@@ -19,14 +52,32 @@ export default function Navbar() {
                                 <Link className="nav-link active" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link active" to="/dashboard">Dashboard</Link>
+                                <Link to="/dashboard" className="nav-link active">DashBoard</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link active" to="/transactions">Transactions</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link active" to="/signup">Sign Up</Link>
-                            </li>
+                            {/* {!isAuthenticated ? <>
+                                <li className="nav-item">
+                                    <Link to="/login" className="btn btn-dark">Login
+
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/signup" className="btn btn-dark">Sign Up
+
+                                    </Link>
+                                </li>
+                            </>
+                                :
+                                <>
+                                    <li className="nav-item">
+                                        <Link to="/dashboard" className="nav-link active">DashBoard</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <button type='button' className="mx-2 btn btn-dark" onClick={handleSignOut}>Logout
+                                        </button>
+                                    </li>
+                                </>
+
+                            } */}
                         </ul>
                     </div>
                 </div>
