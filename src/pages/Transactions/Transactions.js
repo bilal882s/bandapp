0fa64c8c16a125347c38ea2490089b52abee48b9
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-07 11:48:16 +0500

    Update Transactions.js

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -9,19 +9,16 @@ export default function Transactions() {
     <div className='d-flex'>
       <DashboardMenu />
       <div className='center w-100' style={{ height: "60vh" }}>
-        <h1 className="text-center">Add / View Accounts</h1>
+        <h1 className="text-center">View Transactions</h1>
         <div className="container mt-3 text-center">
           <div className="row">
             <Card className="col-12 col-md-12 col-lg-10 offset-lg-1 shadow ">
               <div class="ftext-center">
                 <CardContent>
-                  <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Accounts</h5>
+                  <h4 class="card-title"><i class="fa-solid fa-money-check-dollar mx-1"></i>Transactions</h4>
                   <hr />
-                  <Button className='m-1' size="sm" variant={'contained'} color="success">
-                    <Link className='nav-link' to="/adduser" >Add New Account</Link>
-                  </Button>
                   <Button className='m-1' size="sm" variant={'contained'} color="warning">
-                    <Link className='nav-link' to="/adduser" color='success'>View All Accounts</Link>
+                    <Link className='nav-link' to="" color='success'>View All Transactions</Link>
                   </Button>
                   <hr />
                   <br />
