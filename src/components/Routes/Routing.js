fe30b6ae86ca088a69e75a608cca8baa148d1944
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-08 18:13:00 +0500

    Update Routing.js

--- src/components/Routes/Routing.js
+++ src/components/Routes/Routing.js
@@ -1,16 +1,17 @@
-import React, { useContext } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LandingPage from '../../pages/LandingPage/index';
+import React, { useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LandingPage from "../../pages/LandingPage/index";
 import Transactions from "../../pages/Transactions/Transactions"
-import Signup from '../../pages/signupPage/Signup';
-import Login from '../../pages/signupPage/Login';
-import DashboardMenu from '../../pages/Dashboard/Dashboard';
-import AddAccount from '../../pages/AddAccount/AddAccount';
-import Forget from '../../pages/signupPage/Forget';
+import Signup from "../../pages/signupPage/Signup";
+import Login from "../../pages/signupPage/Login";
+import DashboardMenu from "../../pages/Dashboard/Dashboard";
+import AddAccount from "../../pages/AddAccount/AddAccount";
+import Forget from "../../pages/signupPage/Forget";
 import Accounts from "../../pages/Dashboard/Accounts"
-import AllAccounts from '../../pages/Dashboard/AllAccounts';
-import { AuthContext } from '../../context/Authcontext';
-import PrivateRoute from '../../important/PrivateRoute';
+import AllAccounts from "../../pages/Dashboard/AllAccounts";
+import { AuthContext } from "../../context/Authcontext";
+import PrivateRoute from "../../important/PrivateRoute";
+import View from "../../pages/Transactions/View";
 
 export default function Routing() {
   const { isAuthenticated } = useContext(AuthContext)
@@ -19,29 +20,17 @@ export default function Routing() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/*' element={<LandingPage />} />
-
-        <Route path='/dashboard/transactions' element={<Transactions />} />
-
-        <Route path="/dashboard/allaccounts" element={<AllAccounts />} />
-
-        {/* <Route path='/dashboard' element={<DashboardMenu />} /> */}
-
-        <Route path='/adduser' element={<AddAccount />} />
-        
-        {/* <Route path="/login" element={<Login />} /> */}
-
-        <Route path='/signup' element={<Signup />} />
-
-        <Route path='/dashboard' element={<PrivateRoute Component={DashboardMenu} />} />
-
-        <Route path='/login' element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard"
+        <Route path="/*" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<PrivateRoute Component={DashboardMenu} />} />
+        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard"
           element={<DashboardMenu />} />} />
-
+        <Route path="/dashboard/transactions" element={<Transactions />} />
+        <Route path="/dashboard/transactions/view" element={<View />} />
+        <Route path="/dashboard/allaccounts" element={<AllAccounts />} />
+        <Route path="/dashboard/adduser" element={<AddAccount />} />
+        <Route path="/dashboard/accounts" element={<Accounts />} />
         <Route path="/forget" element={<Forget />} />
-
-        <Route path='/dashboard/accounts' element={<Accounts />} />
-
       </Routes>
 
     </BrowserRouter>
