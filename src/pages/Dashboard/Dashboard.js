d914f1f434e625b40f419241fdb83532763480cb
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-08 18:13:22 +0500

    Update Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -9,7 +9,7 @@ import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 
 export default function Dashboard() {
-  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  const { isAuthenticated, setIsAuthenticated, index, transactions } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -20,51 +20,6 @@ export default function Dashboard() {
   return (
     <div className='d-flex'>
       <DashboardMenu />
-      {/* <div className='m-5' style={{ height: "100vh", width: "100%" }}>
-        <h1 className="text-center">Dashboard Page</h1>
-        <div className="container mt-3 text-center">
-          <div className="row ">
-            <div className="col-12 col-md-12 col-lg-5 shadow ">
-              <div class="ftext-center">
-                <CardContent>
-                  <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Accounts</h5>
-                  <hr />
-                  <Button className='m-1' size="sm" variant={'contained'} color="success">
-                    <Link className='nav-link' to="/adduser" >Add New Account</Link>
-                  </Button>
-                  <Button className='m-1' size="sm" variant={'contained'} color="warning">
-                    <Link className='nav-link' to="/dashboard/allaccounts" color='success'>View All Accounts</Link>
-                  </Button>
-                  <hr />
-                  <br />
-                  <h1>0</h1>
-                  <h4>Accounts</h4>
-                </CardContent>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-md-12 col-lg-5 shadow">
-            <Card>
-              <div class="ftext-center">
-                <CardContent>
-                  <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Accounts</h5>
-                  <hr />
-                  <Button className='m-1' size="sm" variant={'contained'} color="success">
-                    <Link className='nav-link' to="/adduser" >Add New Account</Link>
-                  </Button>
-                  <Button className='m-1' size="sm" variant={'contained'} color="warning">
-                    <Link className='nav-link' to="/adduser" color='success'>View All Accounts</Link>
-                  </Button>
-                  <hr />
-                  <br />
-                  <h1>0</h1>
-                  <h4>Accounts</h4>
-                </CardContent>
-              </div>
-            </Card>
-          </div>
-        </div>
-      </div> */}
       <div className='center w-100' style={{ height: "100vh" }}>
         <h1 className="text-center">Add / View Accounts</h1>
         <div className="container mt-3 text-center">
@@ -76,14 +31,14 @@ export default function Dashboard() {
                     <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Accounts</h5>
                     <hr />
                     <Button className='m-1' size="sm" variant={'contained'} color="success">
-                      <Link className='nav-link' to="/adduser" >Add New Account</Link>
+                      <Link className='nav-link' to="/dashboard/adduser" >Add New Account</Link>
                     </Button>
                     <Button className='m-1' size="sm" variant={'contained'} color="warning">
                       <Link className='nav-link' to="/dashboard/allaccounts" color='success'>View All Accounts</Link>
                     </Button>
                     <hr />
                     <br />
-                    <h1>0</h1>
+                    <h1>{index}</h1>
                     <h4>Accounts</h4>
                   </CardContent>
                 </div>
@@ -96,11 +51,11 @@ export default function Dashboard() {
                     <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Transactions</h5>
                     <hr />
                     <Button className='m-1' size="sm" variant={'contained'} color="warning">
-                      <Link className='nav-link' to="" color='success'>View All Accounts</Link>
+                      <Link className='nav-link' to="/dashboard/transactions/view" color='success'>View All Accounts</Link>
                     </Button>
                     <hr />
                     <br />
-                    <h1>0</h1>
+                    <h1>{transactions}</h1>
                     <h4>Transactions</h4>
                   </CardContent>
                 </div>
