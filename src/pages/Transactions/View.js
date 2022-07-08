6f0243211fb96d04c2da85c683bbccaf57934cc0
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-08 20:05:20 +0500

    Update View.js

--- src/pages/Transactions/View.js
+++ src/pages/Transactions/View.js
@@ -3,24 +3,22 @@ import { AuthContext } from '../../context/Authcontext'
 import DashboardMenu from '../Dashboard/DashboardMenu'
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from "../../config/firebase";
-// import Loading from "../../../96439-loading-bank.json";
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
 
 export default function View() {
-    const { setIndex, user, setTransactions } = useContext(AuthContext);
+    const { user, setTransactions } = useContext(AuthContext);
 
     const [documents, setDocuments] = useState([])
     const [loading, setLoading] = useState(false)
 
     const fetchDocuments = async () => {
         setLoading(true);
-        console.log(user)
         let array = [];
         let price = 0;
         const querySnapshot = await getDocs(collection(db, "Accounts"));
         querySnapshot.forEach((doc) => {
             if (user.uid === doc.data().uid) {
                 price = parseInt(price) + parseInt(doc.data().price);
-                console.log(price);
                 array.push(doc.data());
             }
         })
@@ -37,37 +35,36 @@ export default function View() {
         <>
             {!loading ?
                 <>
-                    <DashboardMenu />
-                    <div className='center' style={{ height: "100vh" }}>
+                    <div className="float-start">
+                        <DashboardMenu />
+                    </div>
+                    <div className='center' style={{ height: "70vh" }}>
                         <h1>All Transactions </h1>
                         <div className="container mt-1 text-center">
                             <div className="row">
                                 <div className="col">
-                                    <div className=" table-responsive ">
-
-                                        <table class="table table-hover">
-                                            <thead className='table-light'>
-                                                <tr>
-                                                    <th scope="col">Number</th>
-                                                    <th scope="col">Name</th>
-                                                    <th scope="col">CNIC No</th>
-                                                    <th scope="col">Price</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {
-                                                    documents.map((item, index) => (
-                                                        <tr>
-                                                            <th>{index + 1}</th>
-                                                            <td>{item.name}</td>
-                                                            <td>{item.cnic}</td>
-                                                            <td>{item.price}</td>
-                                                        </tr>
-                                                    ))
-                                                }
-                                            </tbody>
-                                        </table>
-                                    </div>
+                                    <Table class="table table-hover m-2 me-5" style={{ backgroundColor: "blue", color: "white" }}>
+                                        <Thead className='table-light'>
+                                            <Tr>
+                                                <Th scope="col">Number</Th>
+                                                <Th scope="col">Name</Th>
+                                                <Th scope="col">CNIC No</Th>
+                                                <Th scope="col">Price</Th>
+                                            </Tr>
+                                        </Thead>
+                                        <Tbody>
+                                            {
+                                                documents.map((item, index) => (
+                                                    <Tr>
+                                                        <Th>{index + 1}</Th>
+                                                        <Td>{item.name}</Td>
+                                                        <Td>{item.cnic}</Td>
+                                                        <Td>{item.price}</Td>
+                                                    </Tr>
+                                                ))
+                                            }
+                                        </Tbody>
+                                    </Table>
                                 </div>
                             </div>
                         </div>
