34f73749423fdf074a7a943e2a1494fa4d789157
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-09 15:28:19 +0500

    Update Nopage.js

--- src/pages/Nopage/Nopage.js
+++ src/pages/Nopage/Nopage.js
@@ -1,6 +1,6 @@
 export default function Nopage() {
   return (
-    <div className='text-center text-danger bg'>
+    <div className='text-center text-danger bg' style={{ height: "83vh" }}>
       <h1 className='m-3'>ERROR 404 PAGE NOT FOUND</h1>
     </div>
   )
