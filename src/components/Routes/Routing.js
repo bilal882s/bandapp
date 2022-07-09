5a5ec5b8a631f51d53dedb3e14307468cad97605
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-09 15:28:10 +0500

    Update Routing.js

--- src/components/Routes/Routing.js
+++ src/components/Routes/Routing.js
@@ -22,14 +22,21 @@ export default function Routing() {
       <Routes>
         <Route path="/*" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<PrivateRoute Component={DashboardMenu} />} />
+        <Route path="/dashboard/*" element={<PrivateRoute Component={DashboardMenu} />} />
+        {
+          !isAuthenticated ?
+            <Route path="/dashboard/*" element={<PrivateRoute Component={Login} />} />
+            :
+            <>
+              <Route path="/dashboard/transactions" element={<Transactions />} />
+              <Route path="/dashboard/transactions/view" element={<View />} />
+              <Route path="/dashboard/allaccounts" element={<AllAccounts />} />
+              <Route path="/dashboard/adduser" element={<AddAccount />} />
+              <Route path="/dashboard/accounts" element={<Accounts />} />
+            </>
+        }
         <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard"
           element={<DashboardMenu />} />} />
-        <Route path="/dashboard/transactions" element={<Transactions />} />
-        <Route path="/dashboard/transactions/view" element={<View />} />
-        <Route path="/dashboard/allaccounts" element={<AllAccounts />} />
-        <Route path="/dashboard/adduser" element={<AddAccount />} />
-        <Route path="/dashboard/accounts" element={<Accounts />} />
         <Route path="/forget" element={<Forget />} />
       </Routes>
 
