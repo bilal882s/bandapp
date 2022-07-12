66d9f54951ea0bbe8523911f1d04d05267a38cdc
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-12 21:46:17 +0500

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,5 +1,1 @@
-export default function LandingPage() {   
-            <div className="container w-100">    
-                <div className="row ">   
-                        <div className="card bg-custom custom">   
-                                <h2>Get a $400 bouns then make it better.</h2>   
+import React, { useContext } from 'react';
