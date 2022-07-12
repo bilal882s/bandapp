706212509354e7a41e8b230c8b6a9721243f5c47
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-12 21:46:21 +0500

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,1 +1,8 @@
 import React, { useContext } from 'react';
+            <div className="container w-100"> 
+                <div className="row ">
+                                <h2>Get a $400 bouns then make it better.</h2>
+                    </div>
+            </div>
+    )
+}
