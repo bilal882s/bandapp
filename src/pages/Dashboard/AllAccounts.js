f772ec0aa443b2cba214054503081b8844e718e1
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-12 12:25:54 +0500

    Update AllAccounts.js

--- src/pages/Dashboard/AllAccounts.js
+++ src/pages/Dashboard/AllAccounts.js
@@ -1,19 +1,24 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../context/Authcontext'
 import DashboardMenu from './DashboardMenu'
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore/lite";
 import { db } from "../../config/firebase";
-import Lottie from 'lottie-web';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
 
 export default function AllAccounts() {
     const { setIndex, user } = useContext(AuthContext);
 
     const [documents, setDocuments] = useState([])
+    const [modal, setModal] = useState([]);
     const [loading, setLoading] = useState(false)
 
     const fetchDocuments = async () => {
         setLoading(true);
-        console.log(user)
         let array = [];
         let num = 0;
         const querySnapshot = await getDocs(collection(db, "Accounts"));
@@ -28,55 +33,125 @@ export default function AllAccounts() {
         setLoading(false);
     }
 
-    useEffect(() => {
+    useEffect(() => { 
         fetchDocuments();
     }, [user])
-
+    const data = (item) => {
+        let array = [];
+        array.push(item)
+        setModal(array);
+        console.log(array);
+    }
     return (
-        <>
-            {!loading ?
-                <>
-                    <DashboardMenu />
-                    <div className="container" style={{ marginTop: "4rem" }}>
-                        <div className="row">
-                            <div className="col">
-                                <h1>All Accounts </h1>
-                                <div className="container mt-1 text-center">
-                                    <table class="table table-hover m-2 me-5">
-                                        <thead className='table-dark'>
-                                            <tr>
-                                                <th scope="col">Number</th>
-                                                <th scope="col">Name</th>
-                                                <th scope="col">Account No</th>
-                                                <th scope="col">CNIC No</th>
-                                                <th scope="col">Date</th>
-                                                <th scope="col">Branch No</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody className='bg'>
-                                            {
-                                                documents.map((item, index) => (
-                                                    <tr>
-                                                        <th>{index + 1}</th>
-                                                        <td>{item.name}</td>
-                                                        <td>{item.account}</td>
-                                                        <td>{item.cnic}</td>
-                                                        <td>{item.date}</td>
-                                                        <td>{item.branch}</td>
-                                                    </tr>
-                                                ))
-                                            }
-                                        </tbody>
-                                    </table>
+        <>  {!loading ?
+            <>
+                {
+                    documents.length === 0
+                        ? <>
+                            <DashboardMenu />
+                            <div className='col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 d-flex align-items-center' style={{ height: "65vh" }}>
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col">
+                                            <Card className='shadow-lg'>
+                                                <div class="text-center">
+                                                    <CardContent>
+                                                        <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>You have no any account yet now</h5>
+                                                        <hr />
+                                                        <div className="d-flex justify-content-center">
+                                                            <Link className='nav-link' to="/dashboard/adduser" >
+                                                                <Button className='m-1' size="sm" variant={'contained'} color="success">
+                                                                    Add
+                                                                </Button>
+                                                            </Link>
+                                                        </div>
+                                                    </CardContent>
+                                                    <hr />
+                                                </div>
+                                            </Card>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </> :
-                <div className="d-flex align-items-center text-center justify-content-center" style={{ height: "100vh" }}>
-                    <div className="spinner-border spinner-border-lg"></div>
-                </div>
-            }
+                        </>
+                        :
+                        <>
+
+                            <DashboardMenu />
+                            <div className="container" style={{ marginTop: "4rem" }}>
+                                <div className="row">
+                                    <div className="col">
+                                        <h1>All Accounts </h1>
+                                        <div className="container mt-1 text-center">
+                                            <table className="table table-hover m-2 me-5">
+                                                <thead className='table-dark'>
+                                                    <tr>
+                                                        <th scope="col">Account No</th>
+                                                        <th scope="col">Name</th>
+                                                        <th scope="col">Date</th>
+                                                        <th scope="col">Time</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody className='bg'>
+                                                    {
+                                                        documents.map((item, index) => (
+                                                            <tr>
+                                                                <td>
+                                                                    <div className="btn btn-link" onClick={() => { data(item) }} data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                                        {item.account}
+                                                                    </div>
+                                                                </td>
+                                                                <td>{item.name}</td>
+                                                                <td>{item.date}</td>
+                                                                <td>{item.time}</td>
+                                                            </tr>
+                                                        ))
+                                                    }
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div className="modal-dialog">
+                                    <div className="modal-content">
+                                        <div className="modal-header">
+                                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div className="modal-body">
+                                            <table className="table ">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">Name</th>
+                                                        <th scope="col">Time</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {modal.map((item) => {
+                                                        <tr>
+                                                            <td>{item.name}</td>
+                                                        </tr>
+                                                    })}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div className="modal-footer">
+                                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="button" className="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                }
+            </> :
+            <Box sx={{ width: '100%' }}>
+                <LinearProgress />
+            </Box>
+        }
         </>
     )
 }
