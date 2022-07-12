cbbf5e3cd7db6173b6555be248f11f5701b6f5f8
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-12 12:25:58 +0500

    Update Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import DashboardMenu from "../Dashboard/DashboardMenu";
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from "../../context/Authcontext";
@@ -7,71 +7,98 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 export default function Dashboard() {
-  const { isAuthenticated, setIsAuthenticated, index, transactions } = useContext(AuthContext);
+  const { isAuthenticated, user, setIndex, setIsAuthenticated, index, transactions, setTransactions } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false)
 
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-
-    navigate("/")
+  const fetchDocuments = async () => {
+    setLoading(true);
+    let num = 0;
+    let price = 0;
+    const querySnapshot = await getDocs(collection(db, "Accounts"));
+    querySnapshot.forEach((doc) => {
+      if (user.uid === doc.data().uid) {
+        num = num + 1;
+        price = parseInt(price) + parseInt(doc.data().price);
+      }
+    })
+    setTransactions(price)
+    setIndex(num);
+    setLoading(false);
   }
+
+  useEffect(() => {
+    fetchDocuments();
+  }, [user])
   return (
     <div>
-      <DashboardMenu />
-      <div className='center w-100' style={{ height: "100vh" }}>
-        <h1 className="text-center">Add / View Accounts</h1>
-        <div className="container mt-3 text-center">
-          <div className="row d-flex m-4">
-            <div className="col-12 col-md-6 col-lg-6  ">
-              <Card className='shadow-lg'>
-                <div class="text-center">
-                  <CardContent>
-                    <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Accounts</h5>
-                    <hr />
-                    <div className="d-flex justify-content-center">
-                      <Link className='nav-link' to="/dashboard/adduser" >
-                        <Button className='m-1' size="sm" variant={'contained'} color="success">
-                          Add
-                        </Button>
-                      </Link>
-                      <Link className='nav-link' to="/dashboard/allaccounts" color='success'>
-                        <Button className='m-1' size="sm" variant={'contained'} color="warning">
-                          View
-                        </Button>
-                      </Link>
+      {!loading ?
+        <>
+          <DashboardMenu />
+          <div className='center w-100' style={{ height: "100vh" }}>
+            <h1 className="text-center">Add / View Accounts</h1>
+            <div className="container mt-3 text-center">
+              <div className="row d-flex m-4">
+                <div className="col-12 col-md-6 col-lg-6  ">
+                  <Card className='shadow-lg'>
+                    <div class="text-center">
+                      <CardContent>
+                        <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Accounts</h5>
+                        <hr />
+                        <div className="d-flex justify-content-center">
+                          <Link className='nav-link' to="/dashboard/adduser" >
+                            <Button className='m-1' size="sm" variant={'contained'} color="success">
+                              Add
+                            </Button>
+                          </Link>
+                          <Link className='nav-link' to="/dashboard/allaccounts" color='success'>
+                            <Button className='m-1' size="sm" variant={'contained'} color="warning">
+                              View
+                            </Button>
+                          </Link>
+                        </div>
+                        <hr />
+                        <br />
+                        <h1>{index}</h1>
+                        <h4>Accounts</h4>
+                      </CardContent>
                     </div>
-                    <hr />
-                    <br />
-                    <h1>{index}</h1>
-                    <h4>Accounts</h4>
-                  </CardContent>
+                  </Card>
                 </div>
-              </Card>
-            </div>
-            <div className='col-12 col-md-6 col-lg-6'>
-              <Card className="shadow-lg">
-                <div class="text-center">
-                  <CardContent>
-                    <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Transactions</h5>
-                    <hr />
-                    <Link className='nav-link' to="/dashboard/transactions/view" color='success'>
-                      <Button className='m-1' size="sm" variant={'contained'} color="warning">
-                        View
-                      </Button>
-                    </Link>
-                    <hr />
-                    <br />
-                    <h1>{transactions}</h1>
-                    <h4>Transactions</h4>
-                  </CardContent>
+                <div className='col-12 col-md-6 col-lg-6'>
+                  <Card className="shadow-lg">
+                    <div class="text-center">
+                      <CardContent>
+                        <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>Transactions</h5>
+                        <hr />
+                        <Link className='nav-link' to="/dashboard/transactions/view" color='success'>
+                          <Button className='m-1' size="sm" variant={'contained'} color="warning">
+                            View
+                          </Button>
+                        </Link>
+                        <hr />
+                        <br />
+                        <h1>{transactions}</h1>
+                        <h4>Transactions</h4>
+                      </CardContent>
+                    </div>
+                  </Card>
                 </div>
-              </Card>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+        :
+        <Box sx={{ width: '100%' }}>
+          <LinearProgress />
+        </Box>
+      }
     </div>
   )
 }
