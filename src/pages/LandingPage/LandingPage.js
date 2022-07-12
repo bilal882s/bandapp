203e254868320fe331f7e37f5dffc714c391f4b2
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-12 21:45:26 +0500

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -27,6 +27,7 @@ export default function LandingPage() {
         <div className='bg-image text-white shadow-lg'>   
             <div className="container w-100">    
                 <div className="row ">   
+                    <div className="col-12 col-md-12">   
                         <div className="card bg-custom custom">   
                             <div className="card-body">   
                                 <h2>Get a $400 bouns then make it better.</h2>   
