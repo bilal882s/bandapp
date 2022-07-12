f14e8a654ee44cfd19860662c3490e7826450b77
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-12 21:45:36 +0500

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,20 +1,5 @@
-  
-export default function LandingPage() {  
-    return (  
-            <div className="container w-100">   
-                    <div className="col-12 col-md-12">  
-                        <div className="card bg-custom custom">  
-                                <h2>Get a $400 bouns then make it better.</h2>  
                                 <p>Create an account on <b><i>My Bank</i></b></p>  
-                                <Link to="/" className="btn btn-danger">Home</Link>  
                         </div>  
-                    </div>  
-                </div>  
-            </div>  
-        </div>  
-    )  
-}  
-  import { Link } from "react-router-dom";   
 import React, { useContext } from 'react';   
 import { Link } from "react-router-dom";   
    
