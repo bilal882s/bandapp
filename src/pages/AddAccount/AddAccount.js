20febf7f3baabe422894314b97ef27aecdd0c75a
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-13 21:50:49 +0500

    Update AddAccount.js

--- src/pages/AddAccount/AddAccount.js
+++ src/pages/AddAccount/AddAccount.js
@@ -11,8 +11,6 @@ import { AuthContext } from '../../context/Authcontext';
 import { useNavigate } from 'react-router-dom'; 
 import dayjs from 'dayjs'; 
 import LinearProgress from '@mui/material/LinearProgress'; 
-export default function AddAccount() {
-  const navigate = useNavigate();
   const { uid, setTable } = useContext(AuthContext);
   const initialState = {
     name: "",
