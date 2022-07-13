18d69dc9dc2c5702f8257e0db6602a7f05382696
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-13 21:52:44 +0500

    Update AddAccount.js

--- src/pages/AddAccount/AddAccount.js
+++ src/pages/AddAccount/AddAccount.js
@@ -21,6 +21,10 @@ export default function AddAccount() {
     cnic: "", 
     price: "", 
     branch: "", 
+    currency: "", 
+    uid: "", 
+    date: "", 
+    time: "" 
   }, 
   { 
     label: "Current", 
