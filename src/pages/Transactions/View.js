c3407d345b2a12da736df71fc98bb9e372bf4f6e
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-15 14:50:11 +0500

    Update View.js

--- src/pages/Transactions/View.js
+++ src/pages/Transactions/View.js
@@ -20,10 +20,9 @@ export default function View() {
         setLoading(true);
         let array = [];
         let price = 0;
-        const querySnapshot = await getDocs(collection(db, "Accounts"));
+        const querySnapshot = await getDocs(collection(db, "Amount"));
         querySnapshot.forEach((doc) => {
             if (user.uid === doc.data().uid) {
-                price = parseInt(price) + parseInt(doc.data().price);
                 array.push(doc.data());
             }
         })
@@ -38,75 +37,80 @@ export default function View() {
 
     return (
         <>
-            {documents.length === 0
-                ?
-                <div className='col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 d-flex align-items-center' style={{ height: "65vh" }}>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col">
-                                <Card className='shadow-lg'>
-                                    <div class="text-center">
-                                        <CardContent>
-                                            <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>You have no any account yet now</h5>
-                                            <hr />
-                                            <div className="d-flex justify-content-center">
-                                                <Link className='nav-link' to="/dashboard/adduser" >
-                                                    <Button className='m-1' size="sm" variant={'contained'} color="success">
-                                                        Add
-                                                    </Button>
-                                                </Link>
+            <>
+                {!loading ?
+                    <>
+                        {
+                            documents.length === 0
+                                ?
+                                <>
+                                    <DashboardMenu />
+                                    <div className='col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 d-flex align-items-center' style={{ height: "65vh" }}>
+                                        <div className="container">
+                                            <div className="row">
+                                                <div className="col">
+                                                    <Card className='shadow-lg'>
+                                                        <div class="text-center">
+                                                            <CardContent>
+                                                                <h5 class="card-title"><i class="fa-solid fa-user mb-1 m-2"></i>You have no any account yet now</h5>
+                                                                <hr />
+                                                                <div className="d-flex justify-content-center">
+                                                                    <Link className='nav-link' to="/dashboard/adduser" >
+                                                                        <Button className='m-1' size="sm" variant={'contained'} color="success">
+                                                                            Add
+                                                                        </Button>
+                                                                    </Link>
+                                                                </div>
+                                                            </CardContent>
+                                                            <hr />
+                                                        </div>
+                                                    </Card>
+                                                </div>
                                             </div>
-                                        </CardContent>
-                                        <hr />
+                                        </div>
                                     </div>
-                                </Card>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                :
-                <>
-                    {!loading ?
-                        <> 
-                            <DashboardMenu />
-                            <div className="container" style={{ marginTop: "4rem" }}>
-                                <div className="row">
-                                    <div className="col">
-                                        <h1>All Transactions </h1>
-                                        <table class="table table-hover m-2 me-5">
-                                            <thead className='table-dark'>
-                                                <tr>
-                                                    <th scope="col">Number</th>
-                                                    <th scope="col">Name</th>
-                                                    <th scope="col">CNIC No</th>
-                                                    <th scope="col">Price</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {
-                                                    documents.map((item, index) => (
+                                </>
+                                :
+                                <>
+                                    <DashboardMenu />
+                                    <div className="container" style={{ marginTop: "4rem" }}>
+                                        <div className="row">
+                                            <div className="col">
+                                                <h1>All Transactions </h1>
+                                                <table class="table table-hover m-2 me-5">
+                                                    <thead className='table-dark'>
                                                         <tr>
-                                                            <th>{index + 1}</th>
-                                                            <td>{item.name}</td>
-                                                            <td>{item.cnic}</td>
-                                                            <td>{item.price}</td>
+                                                            <th scope="col">Number</th>
+                                                            <th scope="col">Name</th>
+                                                            <th scope="col">CNIC No</th>
+                                                            <th scope="col">Price</th>
                                                         </tr>
-                                                    ))
-                                                }
-                                            </tbody>
-                                        </table>
+                                                    </thead>
+                                                    <tbody>
+                                                        {
+                                                            documents.map((item, index) => (
+                                                                <tr>
+                                                                    <th>{index + 1}</th>
+                                                                    <td>{item.name}</td>
+                                                                    <td>{item.cnic}</td>
+                                                                    <td>{item.price}</td>
+                                                                </tr>
+                                                            ))
+                                                        }
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        </>
-                        :
-                        <Box sx={{ width: '100%' }}>
-                            <LinearProgress />
-                        </Box>
-                    }
-                </>
-            }
+                                </>
+                        }
+                    </>
+                    :
+                    <Box sx={{ width: '100%' }}>
+                        <LinearProgress />
+                    </Box>
+                }
+            </>
         </>
     )
 }
