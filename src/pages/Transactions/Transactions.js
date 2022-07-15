aaa73bf093af0b0165d7eed83837efe120973ab2
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-15 12:18:10 +0500

    Update Transactions.js

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -30,7 +30,7 @@ export default function Transactions() {
                   <hr />
                   <br />
                   <h1>{transactions}</h1>
-                  <h4>Transactions</h4>
+                  <h4>Total Amount</h4>
                 </CardContent>
               </div>
             </Card>
