495fb415c9de4113b0ae9627b16ffdf6e31c300a
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-17 18:49:30 +0500

    Update View.js

--- src/pages/Transactions/View.js
+++ src/pages/Transactions/View.js
@@ -110,7 +110,7 @@ export default function View() {
                                         </div>
                                     </div>
 
-                                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                    <div class="modal fade bg-light w-100" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                                         <div class="modal-dialog">
                                             <div class="modal-content">
                                                 <div class="modal-header">
