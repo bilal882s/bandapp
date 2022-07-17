04e8158dd6a75dec21f3e3a9e41d4fbdb38771d0
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-17 23:23:21 +0500

    Update Transactions.js

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
