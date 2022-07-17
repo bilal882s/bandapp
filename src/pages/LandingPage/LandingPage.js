c62df6097877218da440e4176eb5224c0d8bb964
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-17 16:57:11 +0500

    Update LandingPage.js

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
+import { Button } from '@mui/material';
+
 export default function LandingPage() {
     return (
         <div className='bg-image text-white shadow-lg'>
-            <div className="container w-100"> 
+            <div className="container w-100">
                 <div className="row ">
                     <div className="col-12 col-md-12">
                         <div className="card bg-custom custom">
                             <div className="card-body">
-                                <h2>Get a $400 bouns then make it better.</h2>
-                                <p>Create an account on <b><i>My Bank</i></b></p>
-                                <Link to="/" className="btn btn-danger">Home</Link>
+                                <h2>My Bank has some Bank Properties</h2>
+                                <p>You can create a bank account on <b><i>My Bank</i></b> and after you can withdraw and deposit your balance.</p>
+                                <Link to="/dashboard" className='nav-link'><Button variant="contained" color="success">Read More</Button></Link>
                             </div>
                         </div>
                     </div>
