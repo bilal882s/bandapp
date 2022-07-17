8ed39896d545034733443e055c804f9c164c1c24
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-17 23:22:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.scss';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react"
+import './App.css';
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Routing from './components/Routes/Routing';
 import AuthContextProvider from './context/Authcontext';
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className='text-center'>
-      <ToastContainer />
+    <div className="text-center">
       <AuthContextProvider>
         <Routing />
       </AuthContextProvider>
+      <ToastContainer />
     </div>
   );
 }
