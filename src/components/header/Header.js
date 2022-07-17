31977c598fcf37f880d59a3fe961c14e2be8a516
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-17 18:48:57 +0500

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { BsFacebook } from "react-icons/bs";
 import { BsWhatsapp } from "react-icons/bs";
@@ -20,13 +20,17 @@ export default function Header() {
                     <div className="col">
                         <div className="topbar text-dark">{time}
                             <div className="float-end mx-5 icons">
-                                <a href="https://www.facebook.com/permalink.php?story_fbid=pfbid0UqYWMXNY3NBeR4ukiYUL6J7mnrF6mGNMz2YzXxBXo4DFoGALqc4gvQ7RSDyUUXhel&id=100078969657877" target="_blank">
+                                <a rel="noreferrer" href="https://www.facebook.com/permalink.php?story_fbid=pfbid0UqYWMXNY3NBeR4ukiYUL6J7mnrF6mGNMz2YzXxBXo4DFoGALqc4gvQ7RSDyUUXhel&id=100078969657877" target="_blank">
                                     <BsFacebook style={{ color: "#1877f2", marginLeft: "2rem", cursor: "pointer" }} />
                                 </a>
                                 <BsWhatsapp style={{ color: "#25d366", marginLeft: "2rem", cursor: "pointer" }} />
                                 <BsInstagram style={{ color: "#405de6", marginLeft: "2rem", cursor: "pointer" }} />
-                                <BsLinkedin style={{ color: "#0077b5", marginLeft: "2rem", cursor: "pointer" }} />
-                                <BsGithub style={{ color: "#333", marginLeft: "2rem", cursor: "pointer" }} />
+                                <a rel="noreferrer" href='https://www.linkedin.com/in/muhammad-bilal-a05200242/' target="_blank">
+                                    <BsLinkedin style={{ color: "#0077b5", marginLeft: "2rem", cursor: "pointer" }} />
+                                </a>
+                                <a rel="noreferrer" href='https://github.com/bilal882s' target="_blank">
+                                    <BsGithub style={{ color: "#333", marginLeft: "2rem", cursor: "pointer" }} />
+                                </a>
                             </div>
                         </div>
                     </div>
