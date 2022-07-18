e5b50499b7287bb747f13b6e095081971acf0506
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-07-18 20:16:01 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
-
 const firebaseConfig = {
     apiKey: "AIzaSyBTDu0XiKEC_7C69_NnxB1f8x8z7oGRdpw",
     authDomain: "ecommerance882.firebaseapp.com",
     projectId: "ecommerance882",
     storageBucket: "ecommerance882.appspot.com",
     messagingSenderId: "270222563905",
-    appId: "1:270222563905:web:d57e6f95ba1ecc29a984c0",
-    measurementId: "G-MPJDWBLGTX"
+    appId: "1:270222563905:web:93307abbcce37cb9a984c0",
+    measurementId: "G-YKBWTC3JG7"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
